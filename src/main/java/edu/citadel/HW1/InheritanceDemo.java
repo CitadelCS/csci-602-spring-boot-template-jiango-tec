68d49db30b089a886f7de2075c1158ff6f7009e8
OscarJGHB <Oscaar.Jiang@outlook.com>
2025-09-19 15:54:06 -0400

    finished hw1

---
+++ src/main/java/edu/citadel/HW1/InheritanceDemo.java
@@ -0,0 +1,48 @@
+package edu.citadel.HW1;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+
+        ArrayList<Employee> employees = new ArrayList<>();
+
+        Employee johnD = new HourlyEmployee("John Doe",LocalDate.of(2009,5,21),50.5, 160.0);
+        Employee janeD = new HourlyEmployee("Jane Doe", LocalDate.of(2005,9,1),150.5,80.0);
+
+        Employee moeH = new SalariedEmployee("Moe Howard", LocalDate.of(2004,1,1),75000.0);
+        Employee curlyH = new SalariedEmployee("Curly Howard", LocalDate.of(2018,1,1),105000.0);
+
+        employees.add(johnD);
+        employees.add(janeD);
+        employees.add(moeH);
+        employees.add(curlyH);
+
+        System.out.println("List of Employees (before sorting):");
+        employees.forEach(employee -> System.out.printf("%s\n",employee));
+
+        System.out.println();
+        Collections.sort(employees);
+
+        System.out.println("List of Employees (after sorting):");
+        employees.forEach(employee -> System.out.printf("%s\n", employee));
+        System.out.println();
+
+        double TotalPay = 0.0;
+        System.out.println("Monthly Pay:");
+        for (Employee employee : employees) {
+            TotalPay += employee.getMonthlyPay();
+            System.out.printf("%s: $%,.2f\n", employee.getName(), employee.getMonthlyPay());
+        }
+        System.out.printf("Total Monthly Pay: %,.2f\n", TotalPay);
+
+
+    }
+
+//    Print o
+//            Print “Monthly Pay” followed by a listing of the monthly pay for each employee followed by the total monthly pay for all employees
+//    Print the monthly pay for each employee using code similar to the following: printf("%s: $%,.2f\n", employee.getName(), employee.getMonthlyPay())
+//    Print the total pay for all employees using code similar to the following: printf("Total Monthly Pay: $%,.2f", totalPay)
+}
